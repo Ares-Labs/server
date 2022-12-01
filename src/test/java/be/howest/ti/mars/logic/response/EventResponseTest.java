64caaf48b9893d6dcc65dfb17676898d195a43f6
Arthurdw <dev@arthurdw.com>
2022-12-01 14:20:14 +0100

    :white_check_mark: Added null checks for the EventResponse

--- src/test/java/be/howest/ti/mars/logic/response/EventResponseTest.java
+++ src/test/java/be/howest/ti/mars/logic/response/EventResponseTest.java
@@ -4,20 +4,31 @@ import be.howest.ti.mars.logic.domain.response.EventResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 class EventResponseTest {
+    private final Map<String, Object> data = Map.of("key", "value");
+
+    @Test
+    void nullEntries() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new EventResponse(null, data));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new EventResponse("type", null));
+    }
+
     @Test
     void getType() {
-        EventResponse eventResponse = new EventResponse("type", null);
+        EventResponse eventResponse = new EventResponse("type", data);
 
         Assertions.assertEquals("type", eventResponse.getType());
     }
 
     @Test
     void manageChannel() {
-        EventResponse eventResponse = new EventResponse("type", null);
+        EventResponse eventResponse = new EventResponse("type", data);
 
         eventResponse.setChannel("my-test-channel");
 
         Assertions.assertEquals("my-test-channel", eventResponse.getChannel());
+
     }
 }
