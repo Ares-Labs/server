fd4cc28858744f301f16e7c8df895e8c2c7af4c0
Arthurdw <dev@arthurdw.com>
2022-12-01 13:26:40 +0100

    :white_check_mark: Users tests

--- src/main/java/be/howest/ti/mars/logic/domain/events/Users.java
+++ src/main/java/be/howest/ti/mars/logic/domain/events/Users.java
@@ -3,7 +3,6 @@ package be.howest.ti.mars.logic.domain.events;
 import be.howest.ti.mars.logic.data.MarsH2Repository;
 import be.howest.ti.mars.logic.data.Repositories;
 import be.howest.ti.mars.logic.domain.response.DataEventResponse;
-import be.howest.ti.mars.logic.domain.response.ErrorEventResponse;
 import be.howest.ti.mars.web.bridge.SocketResponse;
 import io.vertx.core.json.JsonObject;
 
--- src/test/java/be/howest/ti/mars/logic/data/MarsH2RepositoryTest.java
+++ src/test/java/be/howest/ti/mars/logic/data/MarsH2RepositoryTest.java
@@ -1,7 +1,11 @@
 package be.howest.ti.mars.logic.data;
 
+import be.howest.ti.mars.logic.domain.events.Users;
+import be.howest.ti.mars.logic.domain.response.DataEventResponse;
+import be.howest.ti.mars.web.bridge.SocketResponse;
 import io.vertx.core.json.JsonObject;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
@@ -18,5 +22,25 @@ class MarsH2RepositoryTest {
         Repositories.configure(dbProperties);
     }
 
-    // TODO: Test queries
+    private <T, C> void assertInstanceOf(T instance, Class<C> cls) {
+        if (!cls.isInstance(instance)) {
+            throw new AssertionError("Expected instance of " + cls.getName() + " but got " + instance.getClass().getName());
+        }
+    }
+
+    @Test
+    void getUser() {
+        JsonObject data = new JsonObject();
+        data.put("userId", "9a0fbbc6-55f3-11ed-82ca-9313c9a89e82");
+
+        assertInstanceOf(Users.getUser(data), DataEventResponse.class);
+    }
+
+    @Test
+    void getProperties() {
+        JsonObject data = new JsonObject();
+        data.put("userId", "9a0fbbc6-55f3-11ed-82ca-9313c9a89e82");
+
+        assertInstanceOf(Users.getProperties(data), DataEventResponse.class);
+    }
 }
