624cf028a9498dc607f8fd55d616dfaafcc081da
dotlineX <59593757+dotlineX@users.noreply.github.com>
2022-12-20 14:51:46 +0100

    added more dummy users

--- src/main/resources/db-populate.sql
+++ src/main/resources/db-populate.sql
@@ -14,7 +14,27 @@ VALUES ('Camera'),
 INSERT INTO users
 VALUES ('9a0fbbc6-55f3-11ed-82ca-9313c9a89e82', 'John Doe'),
        ('b66e49ae-55f3-11ed-a877-6f6036c8577a', 'Jane Doe'),
-       ('bedb80fc-55f3-11ed-b681-07688aa63f8a', 'Frankenstein');
+       ('bedb80fc-55f3-11ed-b681-07688aa63f8a', 'Frankenstein'),
+       ('9a0fbbc6-55f3-11ed-82ca-9313c9a89e84', 'Mia Johnson'),
+       ('b66e49ae-55f3-11ed-a877-6f6036c8577c', 'Adam Johnson'),
+       ('bedb80fc-55f3-11ed-b681-07688aa63f8c', 'Emma Williams'),
+       ('9a0fbbc6-55f3-11ed-82ca-9313c9a89e85', 'Liam Williams'),
+       ('b66e49ae-55f3-11ed-a877-6f6036c8577d', 'Olivia Brown'),
+       ('bedb80fc-55f3-11ed-b681-07688aa63f8d', 'Noah Brown'),
+       ('9a0fbbc6-55f3-11ed-82ca-9313c9a89e86', 'Ava Jones'),
+       ('b66e49ae-55f3-11ed-a877-6f6036c8577e', 'James Jones'),
+       ('bedb80fc-55f3-11ed-b681-07688aa63f8e', 'Isabella Taylor'),
+       ('9a0fbbc6-55f3-11ed-82ca-9313c9a89e87', 'Oliver Taylor'),
+       ('b66e49ae-55f3-11ed-a877-6f6036c8577f', 'Sophia Davis'),
+       ('bedb80fc-55f3-11ed-b681-07688aa63f8f', 'William Davis'),
+       ('9a0fbbc6-55f3-11ed-82ca-9313c9a89e88', 'Charlotte Anderson'),
+       ('b66e49ae-55f3-11ed-a877-6f6036c85780', 'Jacob Anderson'),
+       ('bedb80fc-55f3-11ed-b681-07688aa63f80', 'Abigail Martinez'),
+       ('9a0fbbc6-55f3-11ed-82ca-9313c9a89e89', 'Michael Martinez'),
+       ('b66e49ae-55f3-11ed-a877-6f6036c85781', 'Emily Thompson'),
+       ('bedb80fc-55f3-11ed-b681-07688aa63f81', 'Benjamin Thompson'),
+       ('9a0fbbc6-55f3-11ed-82ca-9313c9a89e90', 'Elizabeth Jackson'),
+       ('b66e49ae-55f3-11ed-a877-6f6036c85782', 'Daniel Jackson');
 
 -- Create properties
 INSERT INTO properties (location, x, y, width, height, tier, status, description)
